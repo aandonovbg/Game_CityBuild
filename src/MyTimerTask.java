d38d8fe452d783ba4a742d1a2d71210b6de7f9ac
aandonovbg <aandonovbg@gmail.com>
2022-08-01 23:46:40 +0300

    added MyTimerTask() refined validateResources() created deductResources() refined showResources()

---
+++ src/MyTimerTask.java
@@ -0,0 +1,14 @@
+import java.util.TimerTask;
+
+public class MyTimerTask extends TimerTask {
+    protected final int buildersUsed;
+
+    public MyTimerTask(int buildersUsed) {
+        this.buildersUsed = buildersUsed;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
